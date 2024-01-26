ba420a7b73af2a6795dd7df06712d48adb5d6724
Brandon Shellenberger <brandonshellenberger93@gmail.com>
2024-01-25 23:54:39 -0600

    Updated README.md

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # datafun-03-analytics
 
-## Create and activate a project virtual environment
+## Create and activate virtual environment
 
 ```shell
 py -m venv .venv
@@ -10,6 +10,15 @@ py -m venv .venv
 ## Install requests and freeze requirements
 
 ```shell
-py -m pip install requests
+py -m pip install requests pandas numpy
 py -m pip freeze > requirements.txt
 ```
+## Git add, commit, and push
+
+```shell
+git add .
+git commit -m "Commit message here"
+git push origin main
+```
+
+This code is mostly statistical analysis of random baseball factors, except for the txt processing.
--- shellenberger_analytics.py
+++ shellenberger_analytics.py
@@ -10,12 +10,9 @@ import csv
 import json
 from pathlib import Path
 import re
-import math
-import statistics
 
 # import from virtual environment
 import requests
-import logging
 import pandas as pd
 import numpy as np
 
@@ -164,7 +161,12 @@ def fetch_and_write_json_data(folder_name: str, filename: str, url: str):
         print(f"Failed to fetch data: {response.status_code}")
 
 def process_json_data(folder_name: str, filename: str, results: str):
-    
+    '''
+    processes the data.json file and outputs a txt file
+    :param folder_name: Name of the folder to find the file
+    :param filename: Name of the file to open
+    :param results: Name of the file to save the statistical analysis to
+    '''
     with open(Path(folder_name).joinpath(filename), 'r') as file:
         stadium_data = dict(json.load(file))
     
@@ -185,31 +187,36 @@ def main():
     '''
     Main function
     '''
-    #print(f"Name:  {utils.company_name}")
+    print(f"Name:  {utils.company_name}")
     
+    # URLs with differnt data types
     url_text = "https://storage.googleapis.com/kagglesdsdata/datasets/463494/941394/Shakespeare_01.txt?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=gcp-kaggle-com%40kaggle-161607.iam.gserviceaccount.com%2F20240124%2Fauto%2Fstorage%2Fgoog4_request&X-Goog-Date=20240124T015418Z&X-Goog-Expires=259200&X-Goog-SignedHeaders=host&X-Goog-Signature=5d0da122a3e4c7a356b8d4940847e96cc92e695cbd330044d29cc7a752d7810001753662bacdf25b21bfe4d0d3ff633a4b8f3f1ef23bd68afbdbecaa210e4b5f6c6a4614b940fcdc0beded8079a2829c3c38d860b46080222d9262db2f0f1954678287bcbafdf881e757ac9c106c1f271e63859ee5a790d3efa4a678933f11905932e44d61107699118e2bec17cdc8126c8437d422e7239000b9a32c66ee2ad253122c4be55e0ad73265a4fc55b6a3d453ab4bf1e2b82a55b1379bf33818a2cea0acf439669257e3058c7193047714e74a196efeb2eff7fddbed73a14dd22ba99f029f0dca5f55db2796fec7b6e67f33786af37d1781769f75946bc9f791323e"
     url_excel = "https://github.com/mikeygman11/Baseball-Statistics/raw/master/2019mlb.xlsx"
     url_csv = "https://query.data.world/s/352fjx2iiw5427wzmelxeky6fql6uh?dws=00000"
     url_json = "https://storage.googleapis.com/kagglesdsdata/datasets/496274/945103/StadiumsFull.json?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=gcp-kaggle-com%40kaggle-161607.iam.gserviceaccount.com%2F20240124%2Fauto%2Fstorage%2Fgoog4_request&X-Goog-Date=20240124T021508Z&X-Goog-Expires=259200&X-Goog-SignedHeaders=host&X-Goog-Signature=71e775d54b626f4f795aeebb70198e1dad862cb099ea8401a2c8657c507a3040d847a2288ba19b9d37adc9183e978c993c7cf4f7c355ca633c08dcd0d8fa655dc37b8b82a3cf06c23aee21e0452af773f2b2d0fcf19465f682b63d3bb664c88a9ef121685bf163b8291150848dace19a7aa2ecc2507e401d18e81c316a7c7acd61a7ceecbfd3162058bdf3f6065bfc6154e59e7059e38da497d0e638398a34544eff4b32f3255cea82868fa9b3f5fe394abe6ba914489329451dafc667db827161905aa497fc971544cb2b4bf283c9c6e86c7c05bbdb02d00a4be86c127da101aad3a01c1ae6772a1d2518774a284b2b1da3dc3c8398770d749b3a000a348ba0"
     
+    # folder names to write data to
     txt_folder_name = "data-txt"
     excel_folder_name = "data-excel"
     csv_folder_name = "data-csv"
     json_folder_name = "data-json"
     
+    # file names to write data to
     txt_filename = 'data.txt'
     excel_filename = 'data.xls'
     csv_filename = 'data.csv'
     json_filename = 'data.json'
     
-    #fetch_and_write_txt_data(txt_folder_name, txt_filename, url_text)
-    #fetch_and_write_excel_data(excel_folder_name, excel_filename, url_excel)
-    #fetch_and_write_csv_data(csv_folder_name, csv_filename, url_csv)
-    #fetch_and_write_json_data(json_folder_name, json_filename, url_json)
-
-    #process_txt_data(txt_folder_name, txt_filename, 'results_txt.txt')
-    #process_excel_data(excel_folder_name, excel_filename, 'results_excel.txt')
-    #process_csv_data(csv_folder_name, csv_filename, 'results_csv.txt')
+    # fetch data and write to files
+    fetch_and_write_txt_data(txt_folder_name, txt_filename, url_text)
+    fetch_and_write_excel_data(excel_folder_name, excel_filename, url_excel)
+    fetch_and_write_csv_data(csv_folder_name, csv_filename, url_csv)
+    fetch_and_write_json_data(json_folder_name, json_filename, url_json)
+    
+    # process data and write to files as text files
+    process_txt_data(txt_folder_name, txt_filename, 'results_txt.txt')
+    process_excel_data(excel_folder_name, excel_filename, 'results_excel.txt')
+    process_csv_data(csv_folder_name, csv_filename, 'results_csv.txt')
     process_json_data(json_folder_name, json_filename, 'results_json.txt')
 
 
